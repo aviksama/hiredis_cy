d94252792aff849e069cfcb47e084a88acf4ce81
Avik S <avik@deloz.co>
2018-11-23 01:06:11 +0530

    typo fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from Cython.Build import cythonize
 from setuptools import setup
 from setuptools.extension import Extension
 
-main = Extension('cymod', ['cywrap.pyx', 'con.c'], include_dirs=['.'], extra_link_args=[
+main = Extension('cywrap', ['cywrap.pyx', 'con.c'], include_dirs=['.'], extra_link_args=[
     '-O3  -Wall -W -Wstrict-prototypes -Wwrite-strings  -I. '], runtime_library_dirs=['hiredis/'], libraries=['hiredis'])
 sub = Extension('cyredis', ['cyredis.py'], include_dirs=['.'])
 
